0aeedb42191060c9d1c9b75ae473b77a17abb712
charlesmbv <charlierossouw@outlook.com>
2025-06-04 14:02:50 +0200

    Feat: Connect card is a from & docker compose created

--- captive-portal-rd/app/index/connect-card.tsx
+++ captive-portal-rd/app/index/connect-card.tsx
@@ -1,16 +1,34 @@
-"use client"
-
-import { useState } from "react"
 import { Button } from "../components/ui/button"
 import { Checkbox } from "../components/ui/checkbox"
-import { Play, PlayCircleIcon, Wifi } from "lucide-react"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "~/components/ui/form"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 interface ConnectCardProps {
     backgroundImage?: string
 }
 
+export const ConnectFormSchema = z.object({
+    termsAccepted: z.boolean().refine((val) => val === true, {
+        message: "You must accept the terms and conditions to continue",
+    }),
+})
+export type ConnectFormSchemaType = z.infer<typeof ConnectFormSchema>
+
 export default function ConnectCard({ backgroundImage }: ConnectCardProps = {}) {
-    const [accepted, setAccepted] = useState(false)
+
+    const form = useForm<z.infer<typeof ConnectFormSchema>>({
+        resolver: zodResolver(ConnectFormSchema),
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: ConnectFormSchemaType) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        // console.log(values)
+        alert("Connecting to WiFi... Please wait.")
+    }
 
     return (
         <div className="relative bg-[#301358] rounded-3xl w-full max-w-sm mx-auto">
@@ -25,13 +43,6 @@ export default function ConnectCard({ backgroundImage }: ConnectCardProps = {}) 
 
             {/* Main card */}
             <div className="relative rounded-sm p-4 text-white overflow-hidden">
-                {/* Decorative cloud shapes */}
-                {/* <div className="absolute top-4 left-8 w-16 h-8 bg-purple-400/30 rounded-full blur-sm"></div>
-                <div className="absolute top-8 right-12 w-12 h-6 bg-purple-400/25 rounded-full blur-sm"></div>
-                <div className="absolute top-16 left-16 w-10 h-5 bg-purple-400/20 rounded-full blur-sm"></div>
-                <div className="absolute bottom-32 right-8 w-14 h-7 bg-purple-400/25 rounded-full blur-sm"></div>
-                <div className="absolute bottom-40 left-6 w-8 h-4 bg-purple-400/20 rounded-full blur-sm"></div> */}
-
                 {/* WiFi Icon */}
                 <div className="flex justify-center mb-2">
                     {/* <Wifi className="w-12 h-12 text-white" strokeWidth={2.5} /> */}
@@ -46,27 +57,46 @@ export default function ConnectCard({ backgroundImage }: ConnectCardProps = {}) 
                 </div>
 
                 {/* Checkbox */}
-                <div className="flex items-start justify-center space-x-3 mb-4">
-                    <Checkbox
-                        id="terms"
-                        // checked={accepted}
-                        // onCheckedChange={() => setAccepted}
-                        className="border-white data-[state=checked]:bg-white data-[state=checked]:text-[#301358] mt-0.5"
-                    />
-                    <label htmlFor="terms" className="text-sm leading-relaxed cursor-pointer">
-                        Accept terms & conditions to continue
-                    </label>
-                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} >
 
-                {/* CTA Button */}
-                <Button
-                    className="w-full bg-white rounded-4xl hover:bg-gray-100 text-[#301358] font-medium py-6  text-base"
-                // disabled={!accepted}
-                >
-                    {/* <PlayCircleIcon size={70} className="w-5 h-5 mr-2 fill-white text-[#301358]" /> */}
-                    <img src="watch-video-icon.svg" alt="watch video" width={"auth"} height={"auto"} />
-                    Watch video to claim
-                </Button>
+                        {/* <div className=""> */}
+                        <FormField
+                            control={form.control}
+                            name="termsAccepted"
+                            render={({ field }) => (
+                                <FormItem className="flex items-start justify-center space-x-3 mb-4">
+                                    <FormControl>
+                                        <Checkbox
+                                            id="terms"
+                                            checked={field.value}
+                                            onCheckedChange={field.onChange}
+                                            className="border-white data-[state=checked]:bg-white data-[state=checked]:text-[#301358] mt-0.5"
+                                        />
+                                    </FormControl>
+                                    <FormLabel className="text-sm leading-relaxed cursor-pointer">
+                                        Accept terms & conditions to continue
+                                    </FormLabel>
+                                </FormItem>
+                            )}
+                        />
+
+                        {/* <label htmlFor="terms" className="text-sm leading-relaxed cursor-pointer">
+                            Accept terms & conditions to continue
+                        </label> */}
+                        {/* </div> */}
+
+                        {/* CTA Button */}
+                        <Button
+                            className="w-full bg-white rounded-4xl hover:bg-gray-100 text-[#301358] font-medium py-6  text-base"
+                        // disabled={!accepted}
+                        >
+                            {/* <PlayCircleIcon size={70} className="w-5 h-5 mr-2 fill-white text-[#301358]" /> */}
+                            <img src="watch-video-icon.svg" alt="watch video" width={"auth"} height={"auto"} />
+                            Watch video to claim
+                        </Button>
+                    </form>
+                </Form>
             </div>
         </div>
     )
---
+++ captive-portal-rd/compose-down.sh
@@ -0,0 +1,4 @@
+docker compose down -v
+docker container prune
+docker image prune -a
+docker volume prune -a
