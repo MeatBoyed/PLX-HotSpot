b02927952ac8f919d7157ddbfcf5ee4de6aef78e
charlesmbv <charlierossouw@outlook.com>
2025-06-09 19:43:16 +0200

    feat: cleaning & documenting code

--- clients/legacy/captive-portal-and-admin/src/components/home-page/connect-card.tsx
+++ clients/legacy/captive-portal-and-admin/src/components/home-page/connect-card.tsx
@@ -1,13 +1,7 @@
 "use client"
 
-
-import { z } from "zod"
-// import { Form, useForm } from "react-hook-form"
-// import { zodResolver } from "@hookform/resolvers/zod"
-import { Checkbox } from "@radix-ui/react-checkbox"
 import { Button } from "../ui/button"
-// import { FormField, FormItem, FormControl, FormLabel } from "../ui/form"
-import { useEffect, useState } from "react"
+import { useEffect, } from "react"
 import { useRouter } from "next/navigation"
 import Form from "next/form"
 import { loginToHotspot } from "@/lib/mikrotik/mikrotik-service"
@@ -18,29 +12,17 @@ interface ConnectCardProps {
     backgroundImage?: string
 }
 
-export const ConnectFormSchema = z.object({
-    termsAccepted: z.boolean().refine((val) => val === true, {
-        message: "You must accept the terms and conditions to continue",
-    }),
-})
-export type ConnectFormSchemaType = z.infer<typeof ConnectFormSchema>
+// export const ConnectFormSchema = z.object({
+//     termsAccepted: z.boolean().refine((val) => val === true, {
+//         message: "You must accept the terms and conditions to continue",
+//     }),
+// })
+// export type ConnectFormSchemaType = z.infer<typeof ConnectFormSchema>
 const initialState = { success: false, message: "" };
 
 export default function ConnectCard({ backgroundImage }: ConnectCardProps = {}) {
     const router = useRouter()
     const [state, formAction] = useFormState(loginToHotspot, initialState);
-
-    // function onSubmit(event: React.FormEvent<HTMLFormElement>) {
-    //     event.preventDefault()
-    //     if (!termsAccepted) {
-    //         alert("You must accept the terms and conditions to continue")
-    //         return
-    //     }
-    //     // Do something with the form values.
-    //     // ✅ This will be type-safe and validated.
-    //     alert("Connecting to WiFi... Please wait.")
-    //     // router.push("/welcome")
-    // }
 
     // Handles displaying appropriate toast per Action response
     useEffect(() => {
@@ -55,11 +37,6 @@ export default function ConnectCard({ backgroundImage }: ConnectCardProps = {}) 
             router.push("/welcome")
         }
     }, [state])
-    // if (state.success && typeof window !== "undefined") {
-    //     setTimeout(() => {
-    //         window.location.href = "/welcome";
-    //     }, 1000)
-    // }
 
     return (
         <div className="relative bg-[#301358] rounded-3xl w-full max-w-md mx-auto">
@@ -89,49 +66,30 @@ export default function ConnectCard({ backgroundImage }: ConnectCardProps = {}) 
                 </div>
 
                 {/* Checkbox */}
-                {/* <Form {...form}> */}
-                {/* <form onSubmit={form.handleSubmit(onSubmit)} > */}
                 <Form action={formAction}>
                     <div className="flex items-start justify-center space-x-3 mb-4">
-                        {/* <FormField
-                            control={form.control}
-                            name="termsAccepted"
-                            render={({ field }) => ( */}
-                        {/* <FormItem className="flex items-start justify-center space-x-3 mb-4"> */}
-                        {/* <FormControl> */}
                         <input
                             id="terms"
                             type="checkbox"
                             required
                             className="border-white data-[state=checked]:bg-white data-[state=checked]:text-[#301358] mt-0.5"
                         />
-                        {/* </FormControl> */}
-                        {/* <p className="text-sm leading-relaxed cursor-pointer">
-                                Accept terms & conditions to continue
-                            </p> */}
-                        {/* </FormItem> */}
-                        {/* )}
-                        /> */}
-
                         <label htmlFor="terms" className="text-sm leading-relaxed cursor-pointer">
                             Accept terms & conditions to continue
                         </label>
                     </div>
 
-                    {/* CTA Button */}
+                    {/* Connect Button */}
                     <Button
                         className="w-full bg-white rounded-4xl hover:bg-gray-100 text-[#301358] font-medium py-6  text-base hover:cursor-pointer"
                         type="submit"
                         disabled={state.success}
-                    // disabled={!accepted}
                     >
-                        {/* <PlayCircleIcon size={70} className="w-5 h-5 mr-2 fill-white text-[#301358]" /> */}
                         {/* eslint-disable @next/next/no-img-element  */}
                         <img src="watch-video-icon.svg" alt="watch video" width={"auth"} height={"auto"} />
                         Watch video to claim
                     </Button>
                 </Form>
-                {/* </Form> */}
             </div>
         </div>
     )
--- clients/legacy/captive-portal-and-admin/src/lib/mikrotik/mikrotik-service.ts
+++ clients/legacy/captive-portal-and-admin/src/lib/mikrotik/mikrotik-service.ts
@@ -2,56 +2,7 @@
 "use server"
 
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import { MikroTikData, MikroTikDataSchema } from "./mikrotik-types";
-
-// type LoginParams = {
-//     username: string;
-//     password: string;
-//     host?: string;
-// }
-
-// // export async function loginToHotspot(): Promise<{ success: boolean; response: string }> {
-// export async function loginToHotspot() {
-//     const username = "click_to_connect@dev"
-//     const password = "click_to_connect"
-//     const host = "10.5.50.1"
-
-//     const mikrotikRaw = await getMikroTikDataFromCookie();
-//     if (!mikrotikRaw) {
-//         console.error("No MikroTik data found in cookies");
-//         return redirect('/error?message=No MikroTik data found');
-//     }
-//     console.log("Raw data: ", mikrotikRaw)
-
-//     const query = new URLSearchParams({
-//         username,
-//         password,
-//     });
-//     // const url = `http://${host}/login?${query.toString()}`;
-//     const url = `${mikrotikRaw.loginlink}?${query.toString()}`;
-
-//     try {
-//         // throw Error("Test error")
-//         const res = await fetch(url, {
-//             method: 'GET',
-//             redirect: 'follow',
-//         });
-
-//         const text = await res.text();
-
-//         const success = res.ok && /already|logged/i.test(text);
-//         console.log(`Success: ${success}, Response: ${text}`);
-//         // return { success, response: text };
-//     } catch (err) {
-//         console.error('MikroTik login error:', err);
-//         console.log(`Success: false, Response: ${(err as Error).message}`);
-//         // return { success: false, response: (err as Error).message };
-//     }
-
-//     redirect('/welcome');
-// }
-
 
 type LoginFormState = {
     success: boolean;
@@ -59,14 +10,17 @@ type LoginFormState = {
 };
 
 export async function loginToHotspot(_: any, formData: FormData): Promise<LoginFormState> {
+    // Default credentials (No Registration)
     const username = "click_to_connect@dev";
     const password = "click_to_connect";
 
+    // Get Session data provided by Mikrotik Hotspot
     const mikrotikRaw = await getMikroTikDataFromCookie();
     if (!mikrotikRaw) {
         return { success: false, message: "Hotspot data missing or invalid. Please try again. TODO: Block rendering if no data is passed/found" };
     }
 
+    // Use provided Mikrotik login link and supply credentials
     const url = `${mikrotikRaw.loginlink}?${new URLSearchParams({ username, password })}`;
 
     try {
@@ -86,6 +40,7 @@ export async function loginToHotspot(_: any, formData: FormData): Promise<LoginF
     }
 }
 
+// Handles retrieving Mikrotik data from cookie, validating, typing and returning it
 export async function getMikroTikDataFromCookie(): Promise<MikroTikData | null> {
     const cookieStore = await cookies();
     const raw = cookieStore.get("mikrotik-data")?.value;
