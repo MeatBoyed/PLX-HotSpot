5e8f6e11106d44581e769ed06ca236f07377f5a8
charlesmbv <charlierossouw@outlook.com>
2025-06-09 19:42:35 +0200

    feat: Toast Error handling, and spam prevention

--- next-captive-portal-rd/src/app/layout.tsx
+++ next-captive-portal-rd/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AdBanner from "@/components/home-page/ad-banner";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
           {children}
           {/* Ads Manager */}
           <AdBanner />
+          <Toaster position="top-center" richColors />
         </div>
       </body>
     </html>
