f7baac910ae9d2019d341e1316a0949e6e6b76ef
charlesmbv <charlierossouw@outlook.com>
2025-06-19 09:40:33 +0200

    feat: demo-stable portal

--- clients/legacy/captive-portal-and-admin/src/app/welcome/page.tsx
+++ clients/legacy/captive-portal-and-admin/src/app/welcome/page.tsx
@@ -2,21 +2,55 @@
 "use server"
 
 import UserSession from '@/components/welcome-page/user-session';
-import { getMikroTikDataFromCookie } from '@/lib/mikrotik/mikrotik-lib';
+// import { getMikroTikDataFromCookie } from '@/lib/mikrotik/mikrotik-lib';
 import { getUserSession } from '@/lib/mikrotik/mikrotik-service';
-import { MikroTikData } from '@/lib/mikrotik/mikrotik-types';
+import { MikroTikData, StatusResponse } from '@/lib/mikrotik/mikrotik-types';
 import { redirect } from 'next/navigation';
+
+const mikrotikRaw: MikroTikData = {
+    loginlink: 'http://charles.hotspot/login',
+    nasid: 'Charles-MT',
+    link_status: 'http://charles.hotspot/status',
+    link_login_only: 'http://charles.hotspot/login',
+    link_logout: 'http://charles.hotspot/logout',
+    mac: '98%3ABD%3A80%3ACE%3AD8%3A35',
+    type: 'mikrotik',
+    ssid: 'dev'
+
+}
+
+const userSession: StatusResponse = {
+    success: true,
+    data: {
+        logged_in: "true",
+        username: "testuser",
+        ip: "38.0.101.76",
+        mac: "98%3ABD%3A80%3ACE%3AD8%3A35",
+        bytes_in: "0",
+        bytes_out: "0",
+        bytes_in_nice: "0",
+        bytes_out_nice: "0",
+        packets_in: "0",
+        packets_out: "0",
+        uptime: "0",
+        session_time_left: "0",
+        remain_bytes_in: "0",
+        remain_bytes_out: "0",
+        link_login_only: "http://charles.hotspot/login",
+        link_logout: "http://charles.hotspot/logout"
+    }
+}
 
 export default async function WelcomePage() {
     // Get Posted Mikrotik Data
-    const mikrotikRaw = await getMikroTikDataFromCookie();
+    // const mikrotikRaw = await getMikroTikDataFromCookie();
     if (!mikrotikRaw) {
         redirect("/");
     }
     console.log("Raw data: ", mikrotikRaw)
 
 
-    const userSession = await getUserSession(mikrotikRaw as MikroTikData);
+    // const userSession = await getUserSession(mikrotikRaw as MikroTikData);
     if (userSession.success && userSession.data) {
         console.log("Hotspot Status:", userSession.data);
     } else {
@@ -48,7 +82,6 @@ export default async function WelcomePage() {
 
             <main className="flex items-start justify-start bg-white w-full rounded-t-[20px] pt-3 pb-10 md:items-center md:justify-center max-w-md">
                 {mikrotikRaw && (
-
                     <UserSession status={userSession.data} />
                 )}
             </main>
