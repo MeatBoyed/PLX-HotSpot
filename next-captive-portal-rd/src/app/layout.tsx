38e9ec7bb65efa7d4ef1b8596d5890e2f9f35e44
charlesmbv <charlierossouw@outlook.com>
2025-06-30 13:09:12 +0200

    Touch ups (not tested)

--- next-captive-portal-rd/src/app/layout.tsx
+++ next-captive-portal-rd/src/app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AdBanner from "@/components/home-page/ad-banner";
 import { Toaster } from "sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "PluxNet Fibre HotSpot",
@@ -27,7 +27,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gray-50`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gray-50`}
+        className={`antialiased min-h-screen bg-gray-50`}
       >
         <div className="flex flex-col justify-between min-h-screen">
 
