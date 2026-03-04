53788a5d7730882d3615d4b513b9d7b81e3497c5
charlesmbv <charlierossouw@outlook.com>
2026-03-05 00:29:41 +0200

    feat: autoconnect resolved

--- clients/legacy/captive-portal-and-admin/next.config.ts
+++ clients/legacy/captive-portal-and-admin/next.config.ts
@@ -48,6 +48,14 @@ const nextConfig: NextConfig = {
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
+      }, {
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "http://ads.pluxnet.co.za" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
       },
     ];
   }
