f674aa0db701a5f0ef06cdbc862f2b3c5835d651
charlesmbv <charlierossouw@outlook.com>
2026-05-23 12:59:14 +0200

    Update AdSection margin for improved layout

--- next-captive-portal-rd/src/components/ad-section.tsx
+++ next-captive-portal-rd/src/components/ad-section.tsx
@@ -2,7 +2,7 @@ import AdBanner from '@/components/revive/ad-banner';
 
 export default function AdSection() {
     return (
-        <section className="w-full flex items-center justify-center">
+        <section className="w-full flex items-center justify-center mt-10">
             <div className="w-full max-w-md">
                 <div className="w-full relative">
                     <AdBanner />
